6816955607a6468e5163a1b6b0d2c2e3ffd7237b
herbert <mail@lischka-berlin.de>
2017-11-01 12:06:26 +0100

    rewrite getBacnetClient to class BacnetClient

--- test/BacnetClientTests.js
+++ test/BacnetClientTests.js
@@ -14,12 +14,16 @@ suite('BacnetClient', () => {
       done();
     });
 
-    test('close releases port', done => {
-      const client = new BacnetClient();
+    test('whoIs fires event iAm', done => {
       let eventFired = false;
+      const client = new BacnetClient();
 
+      // client.me.on('iAm', device => this.discoverDevice(device));
       client.on('iAm', () => {
         eventFired = true;
+
+        // console.log('iAm event received', device);
+        done();
       });
 
       setTimeout(() => {
@@ -30,7 +34,28 @@ suite('BacnetClient', () => {
       }, 2500);
 
       client.whoIs();
-      done();
+    });
+
+    test.skip('discoverDevice fires event', done => {
+      let eventFired1 = false;
+      const client = new BacnetClient();
+
+      // client.me.on('iAm', device => this.discoverDevice(device));
+      client.on('gotDevice', () => {
+        eventFired1 = true;
+        done();
+
+        // console.log('iAm received', device);
+      });
+
+      setTimeout(() => {
+        assert.that(eventFired1, 'Event did not fire within 2 seconds').is.true();
+      }, 2000);
+      setTimeout(() => {
+        client.close();
+      }, 2500);
+
+      // client.discoverDevice({ address: '192.168.178.9' });
     });
   });
 });
