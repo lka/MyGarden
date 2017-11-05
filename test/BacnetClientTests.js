672c92bbdd708393120dd01110dd687fdd097fbc
herbert <mail@lischka-berlin.de>
2017-11-05 15:08:10 +0100

    tried to find a stub for UDP

--- test/BacnetClientTests.js
+++ test/BacnetClientTests.js
@@ -2,6 +2,9 @@
 
 const assert = require('assertthat'),
       mockudp = require('mock-udp');
+const util = require('util');
+
+const dgram = require('dgram');
 
 const BacnetClient = require('../lib/BacnetClient');
 
@@ -30,6 +33,33 @@ suite('BacnetClient', () => {
 
       assert.that(scope.done(), 'nothing was sent').is.true();
       mockudp.revert();
+      done();
+    });
+
+    test.skip('iAm was received', done => {
+      const udpclient = dgram.createSocket('udp4');
+      const buffer = Buffer.from([ 0x81, 0x0b, 0x00, 0x18,
+        0x01, 0x20, 0xff, 0xff, 0x00, 0xff,
+        0x10, 0x00, 0xc4, 0x02, 0x00, 0x00, 0x63, 0x22, 0x01, 0xe0, 0x91, 0x00, 0x21, 0x27 ]);
+
+      const rinfo = { address: '192.168.178.255',
+        family: 'IPv4',
+        port: 47808,
+        size: buffer.length };
+
+      // client.emit('message', buffer, rinfo);
+
+      udpclient.send(buffer, 0, buffer.length, rinfo.port, rinfo.address, (err, bytes) => {
+        if (err) {
+          throw new Error(err);
+        }
+        /* eslint-disable no-console */
+        console.log(`UDP message received from ${rinfo.address}:${rinfo.port}`);
+        console.log('EventListeners: ', util.inspect(client.listenerCount('message')));
+        console.log('Socket: ', util.inspect(client.address()));
+        /* eslint-enable no-console */
+        udpclient.close();
+      });
       done();
     });
 
