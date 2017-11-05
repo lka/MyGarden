b52131250ba24f02ab8639ea4818b0ab1b770a5a
herbert <mail@lischka-berlin.de>
2017-11-05 15:34:08 +0100

    mock-dgram ??

--- test/BacnetClientTests.js
+++ test/BacnetClientTests.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const assert = require('assertthat'),
+      // io = require('socket.io').listen(47808),
+      // MockDgram = require('mock-dgram'),
       mockudp = require('mock-udp');
 const util = require('util');
 
-const dgram = require('dgram');
+// const dgram = require('dgram');
 
 const BacnetClient = require('../lib/BacnetClient');
 
@@ -32,35 +34,59 @@ suite('BacnetClient', () => {
       // console.log(scope.buffer);
 
       assert.that(scope.done(), 'nothing was sent').is.true();
+      assert.that(new Uint8Array(scope.buffer), 'buffer contains whoIs').is.equalTo(new Uint8Array([ 129, 11, 0, 8, 1, 0, 16, 8 ]));
       mockudp.revert();
       done();
     });
 
-    test.skip('iAm was received', done => {
-      const udpclient = dgram.createSocket('udp4');
+    test('iAm was received', done => {
+      // const udpclient = dgram.createSocket('udp4');
+      // const md = new MockDgram();
       const buffer = Buffer.from([ 0x81, 0x0b, 0x00, 0x18,
         0x01, 0x20, 0xff, 0xff, 0x00, 0xff,
         0x10, 0x00, 0xc4, 0x02, 0x00, 0x00, 0x63, 0x22, 0x01, 0xe0, 0x91, 0x00, 0x21, 0x27 ]);
 
-      const rinfo = { address: '192.168.178.255',
+      // const msgIn = {
+      //   ip: { src: '192.168.178.9' },
+      //   udp: { srcPort: 47808, dataLength: buffer.length },
+      //   data: buffer
+      // };
+
+      const rinfo = { address: '192.168.178.9',
         family: 'IPv4',
         port: 47808,
         size: buffer.length };
 
-      // client.emit('message', buffer, rinfo);
+      let eventFired = false;
 
-      udpclient.send(buffer, 0, buffer.length, rinfo.port, rinfo.address, (err, bytes) => {
-        if (err) {
-          throw new Error(err);
-        }
-        /* eslint-disable no-console */
-        console.log(`UDP message received from ${rinfo.address}:${rinfo.port}`);
-        console.log('EventListeners: ', util.inspect(client.listenerCount('message')));
-        console.log('Socket: ', util.inspect(client.address()));
-        /* eslint-enable no-console */
-        udpclient.close();
+      // client.me.on('iAm', device => this.discoverDevice(device));
+      client.once('iAm', () => {
+        eventFired = true;
+        done();
       });
-      done();
+
+      console.log('methods: ', BacnetClient[]);
+      console.log('EventListeners: ', client.listenerCount('message'));
+
+      setTimeout(() => {
+        assert.that(eventFired, 'Event did not fire within 2 seconds').is.true();
+      }, 2000);
+
+      client.emit('message', buffer, rinfo);
+
+      // md.input.write(msgIn);
+      // udpclient.send(buffer, 0, buffer.length, rinfo.port, rinfo.address, (err, bytes) => {
+      //   if (err) {
+      //     throw new Error(err);
+      //   }
+        /* eslint-disable no-console */
+        // console.log(`UDP message received from ${rinfo.address}:${rinfo.port}`);
+        console.log('EventListeners: ', util.inspect(client.listenerCount('message')));
+        // console.log('Socket: ', util.inspect(client.address()));
+        /* eslint-enable no-console */
+        // udpclient.close();
+      // });
+      // done();
     });
 
     test.skip('whoIs fires event iAm', done => {
