77bd641ca08997b8a1b75d8c78d25542a397c56e
hlischka <hlischka@MGH.KLINIKEN-DR-VOETISCH.DE>
2017-11-06 15:49:10 +0100

    stub with receiveData

--- test/BacnetClientTests.js
+++ test/BacnetClientTests.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const assert = require('assertthat'),
-      // io = require('socket.io').listen(47808),
-      // MockDgram = require('mock-dgram'),
       mockudp = require('mock-udp');
-const util = require('util');
 
 // const dgram = require('dgram');
 
@@ -20,102 +17,57 @@ suite('BacnetClient', () => {
       }, 2500);
     });
 
-    test.skip('BacnetClient returns a function', done => {
-      assert.that(typeof client).is.ofType('object');
-      done();
-    });
-
     test('whoIs was sent', done => {
       // Create scope to capture UDP requests
       const scope = mockudp('255.255.255.255:47808');
 
       client.whoIs();
 
-      // console.log(scope.buffer);
-
-      assert.that(scope.done(), 'nothing was sent').is.true();
-      assert.that(new Uint8Array(scope.buffer), 'buffer contains whoIs').is.equalTo(new Uint8Array([ 129, 11, 0, 8, 1, 0, 16, 8 ]));
+      assert.that(scope.done()).is.true();
+      assert.that(new Uint8Array(scope.buffer)).is.equalTo(new Uint8Array([ 129, 11, 0, 8, 1, 0, 16, 8 ]));
       mockudp.revert();
       done();
     });
 
-<<<<<<< HEAD
-    test('iAm was received', done => {
-      // const udpclient = dgram.createSocket('udp4');
-      // const md = new MockDgram();
-=======
-    test.skip('iAm was received', done => {
-      // const udpclient = dgram.createSocket('udp4');
-      const md = new MockDgram();
->>>>>>> fdd68a019b70b76744c33920baee11f2875c37c1
+    test('iAm was received and readPropertyMultiple was sent', done => {
+      const address = '192.168.178.9';
+
+      // Create scope to capture UDP readPropertyMultiple request
+      const scope = mockudp(`${address}:47808`);
+
       const buffer = Buffer.from([ 0x81, 0x0b, 0x00, 0x18,
         0x01, 0x20, 0xff, 0xff, 0x00, 0xff,
         0x10, 0x00, 0xc4, 0x02, 0x00, 0x00, 0x63, 0x22, 0x01, 0xe0, 0x91, 0x00, 0x21, 0x27 ]);
 
-<<<<<<< HEAD
-      // const msgIn = {
-      //   ip: { src: '192.168.178.9' },
-      //   udp: { srcPort: 47808, dataLength: buffer.length },
-      //   data: buffer
-      // };
-
-      const rinfo = { address: '192.168.178.9',
-=======
-      const msgIn = {
-        ip: { src: '192.168.178.9' },
-        udp: { srcPort: 47807, dataLength: buffer.length },
-        data: buffer
-      };
-
-      const rinfo = { address: '192.168.178.255',
->>>>>>> fdd68a019b70b76744c33920baee11f2875c37c1
-        family: 'IPv4',
-        port: 47808,
-        size: buffer.length };
-
       let eventFired = false;
 
-      // client.me.on('iAm', device => this.discoverDevice(device));
+      mockudp.intercept();
+
       client.once('iAm', () => {
         eventFired = true;
+        assert.that(scope.done()).is.true();
+        assert.that(new Uint8Array(scope.buffer)).is.equalTo(new Uint8Array([ 129, 10, 0, 19, 1, 4, 2, 117, 0, 14, 12, 2, 63, 255, 255, 30, 9, 8, 31 ]));
+        mockudp.revert();
         done();
       });
 
-      console.log('methods: ', BacnetClient[]);
-      console.log('EventListeners: ', client.listenerCount('message'));
-
       setTimeout(() => {
-        assert.that(eventFired, 'Event did not fire within 2 seconds').is.true();
+        assert.that(eventFired).is.true();
       }, 2000);
 
-      client.emit('message', buffer, rinfo);
-
-      // md.input.write(msgIn);
-      // udpclient.send(buffer, 0, buffer.length, rinfo.port, rinfo.address, (err, bytes) => {
-      //   if (err) {
-      //     throw new Error(err);
-      //   }
-        /* eslint-disable no-console */
-        // console.log(`UDP message received from ${rinfo.address}:${rinfo.port}`);
-        console.log('EventListeners: ', util.inspect(client.listenerCount('message')));
-        // console.log('Socket: ', util.inspect(client.address()));
-        /* eslint-enable no-console */
-        // udpclient.close();
-      // });
-      // done();
+      client.receiveData(buffer, address);
     });
 
     test.skip('whoIs fires event iAm', done => {
       let eventFired = false;
 
-      // client.me.on('iAm', device => this.discoverDevice(device));
       client.once('iAm', () => {
         eventFired = true;
         done();
       });
 
       setTimeout(() => {
-        assert.that(eventFired, 'Event did not fire within 2 seconds').is.true();
+        assert.that(eventFired).is.true();
       }, 2000);
 
       client.whoIs();
@@ -130,7 +82,7 @@ suite('BacnetClient', () => {
       });
 
       setTimeout(() => {
-        assert.that(eventFired1, 'Event did not fire within 2 seconds').is.true();
+        assert.that(eventFired1).is.true();
       }, 2000);
 
       client.whoIs();
